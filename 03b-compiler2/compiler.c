6d61eec6c4a3a30bf4256100eb4e84be250d68f3
ZKX-0326 <relax90326@gmail.com>
2021-05-05 14:54:17 +0800

    add sp109b

--- 03b-compiler2/compiler.c
+++ 03b-compiler2/compiler.c
@@ -122,15 +122,15 @@ void IF(){
   skip("if");
   skip("(");
   int e = E();
-  emit("if not T%d goto L%d\n", e, ifMid);
+  emit("if not T%d goto L%d\n", e, ifMid); //若判斷式不成立則進入下一個條件式
   skip(")");
   STMT();
-  emit("goto L%d\n", ifEnd);
+  emit("goto L%d\n", ifEnd); //表示完成條件式中的code，並結束整個條件式
   emit("(L%d)\n", ifMid);
-  if(isNext("else")){
+  if(isNext("else")){ //else if和else的條件式
     skip("else");
     STMT();
-    emit("(L%d)\n",ifEnd);
+    emit("(L%d)\n",ifEnd); //表示完成條件式中的code，並結束整個條件式
   }
 }
 
